3daff23af192e55809a08eedbb10eb3137e2669a
Alisson Youssf <youssfbr@gmail.com>
2022-04-19 20:06:10 -0300

    Testando a primeira classe de dominio - Categoria

--- backend/src/main/java/com/github/youssfbr/sistema/controllers/CategoriaController.java
+++ backend/src/main/java/com/github/youssfbr/sistema/controllers/CategoriaController.java
@@ -1,2 +1,27 @@
-package com.github.youssfbr.sistema.controllers;public class CategoriaController {
+package com.github.youssfbr.sistema.controllers;
+
+import com.github.youssfbr.sistema.entities.Categoria;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categorias")
+public class CategoriaController {
+
+    @GetMapping
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1L, "Informática");
+        Categoria cat2 = new Categoria(2L, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
+    }
 }
---
+++ backend/src/main/java/com/github/youssfbr/sistema/entities/Categoria.java
@@ -0,0 +1,43 @@
+package com.github.youssfbr.sistema.entities;
+
+import lombok.*;
+
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Table(name = "tb_categorias")
+public class Categoria  implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Categoria client = (Categoria) o;
+        return id != null && Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+
+}
