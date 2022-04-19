87a819a8bf578e1a01f69102e6cc6eb8c845e666
Alisson Youssf <youssfbr@gmail.com>
2022-04-19 17:30:19 -0300

    Projeto criado

---
+++ backend/src/test/java/com/github/youssfbr/sistema/SistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
